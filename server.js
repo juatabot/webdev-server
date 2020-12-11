7fc692f359b8db0da02d743a1b5c0640539e7186
Nikita Uppal <nikitauppal29@gmail.com>
2020-12-10 20:06:59 -0500

    cross origin

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require("express")
 const fetch = require("node-fetch");
 const app = express()
 
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:4200", // restrict calls to those this address
+    methods: "GET, POST, PUT, DELETE" // only allow GET requests
+  })
+);
+
 require("./controllers/stocks.js")(app)
 require("./controllers/portfolio.js")(app)
 require("./controllers/trades.js")(app)
