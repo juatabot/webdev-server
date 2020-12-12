c51d79187b72d979b4d05938f1fb8a8a9f7a0ba7
juatabot <hengjuat@gmail.com>
2020-12-11 16:12:00 -0800

    Add login/register connection to sqlite

---
+++ controllers/login.js
@@ -0,0 +1,26 @@
+const service = require("../services/login/login_db.js")
+module.exports = (app) => {
+    const register = (req, res) => {
+        service.register(req.body)
+            .then(resp => { res.send(resp) })
+    }
+
+    const login = (req, res) => {
+        service.findUserByCredentials(req.body)
+            .then(resp => { res.send(resp) })
+    }
+
+    const currentUser = (req, res) =>
+        res.json(req.session["currentUser"])
+
+    const logout = (req, res) => {
+        req.session.destroy()
+        res.send(200)
+    }
+
+    app.post('/api/login', login)
+    app.post('/api/register', register)
+    app.post('/api/currentUser',
+        currentUser)
+    app.post('/api/logout', logout)
+}
---
+++ services/login/login_db.js
@@ -0,0 +1,28 @@
+const db = require('../../db.js')
+
+const register = (body) => {
+    const query = `INSERT INTO user (username, pwd) VALUES (${body.username}, ${body.pwd})`;
+    return new Promise((resolve) => {
+        db.run(query, [], (result, err) => {
+            if (err) {
+                throw err;
+            }
+            // Return ID of object inserted!!!
+            resolve(result);
+        });
+    })
+}
+
+const findUserByCredentials = (body) => {
+    const query = `SELECT * FROM user WHERE user.username = ${body.username} AND user.pwd = ${body.pwd}`;
+    return new Promise((resolve) => {
+        db.all(query, [], (err, rows) => {
+            if (err) {
+                throw err;
+            }
+            resolve(rows);
+        });
+    })
+}
+
+module.exports = { register, findUserByCredentials }
